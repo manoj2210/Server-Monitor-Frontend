1f73ba0b0229d9a6a09418e573b8277b93521cec
Manoj2210 <manojkumarmaths1000@gmail.com>
2020-03-19 16:38:31 +0530

    Network Page Completed

--- ui/src/pages/network.js
+++ ui/src/pages/network.js
@@ -1,16 +1,100 @@
 import { Component } from "react";
 import Layout from "../components/layout"
+import { get } from "../../services/rest_service"
 
 export default class Network extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            conn:[],
-            rout:[],
-            actconn:[]
+        this.state = {
+            conn: [],
+            rout: [],
+            actconn: []
         }
+        this.handleHardwareInfo();
+    }
+    handleHardwareInfo = e => {
+        get("http://127.0.0.1:8080/network").then(m => {
+            if (m != "ERR") {
+                this.setState({ conn: m.conn, rout: m.route_table, actconn: m.active_conn });
+            }
+        })
+
     }
     render() {
+        let conn;
+        if (this.state.conn) {
+            let m = this.state.conn;
+            conn = m.map((d, index) => (
+                <tr>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.name}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.uuid}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.type}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.device}</p>
+                    </td>
+                </tr >
+            )
+            );
+        }
+
+        let rout;
+        if (this.state.rout) {
+            let m = this.state.rout;
+            rout = m.map((d, index) => (
+                <tr>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.destination}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.gateway}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.genmask}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.device}</p>
+                    </td>
+                </tr >
+            )
+            );
+        }
+
+        let actconn;
+        if (this.state.actconn) {
+            let m = this.state.actconn;
+            actconn = m.map((d, index) => (
+                <tr>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.protocol}</p>
+                    </td>
+                    {/* <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.datasr}</p>
+                    </td> */}
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.local_address}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.foreign_address}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.state}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.pid}</p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-gray-700 text-sm">
+                        <p className="text-gray-400 whitespace-no-wrap">{d.process_name}</p>
+                    </td>
+                </tr >
+            )
+            );
+        }
         return (
             <Layout>
                 <div class="container w-full mx-auto pt-20">
@@ -53,7 +137,7 @@ export default class Network extends Component {
                                             </tr>
                                         </thead>
                                         <tbody>
-
+                                            {conn}
                                         </tbody>
                                     </table>
                                 </div>
@@ -99,7 +183,7 @@ export default class Network extends Component {
                                             </tr>
                                         </thead>
                                         <tbody>
-
+                                            {rout}
                                         </tbody>
                                     </table>
                                 </div>
@@ -124,11 +208,11 @@ export default class Network extends Component {
                                                         Protocol
                                             </p>
                                                 </th>
-                                                <th
+                                                {/* <th
                                                     className="px-5 py-5 border-b-2 border-gray-200 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider"
                                                 >
                                                     Data Sent/Received
-                                            </th>
+                                            </th> */}
                                                 <th
                                                     className="px-5 py-5 border-b-2 border-gray-200 bg-gray-900 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
                                                 >
@@ -167,7 +251,7 @@ export default class Network extends Component {
                                             </tr>
                                         </thead>
                                         <tbody>
-
+                                            {actconn}
                                         </tbody>
                                     </table>
                                 </div>
