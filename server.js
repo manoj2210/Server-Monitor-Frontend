580f14c5a5e7cdf3f8f6ee7900c49da5a35270ee
Manoj2210 <manojkumarmaths1000@gmail.com>
2020-03-31 19:29:51 +0530

    yarn removed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const next = require('next');
-const port = 3000;
+const port = 80;
 const dev = 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
